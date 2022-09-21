c1d04fac9bcedd9d3ed8bce44111c9b70e105418
Damon <nomad.damon@gmail.com>
2022-09-21 10:06:52 -0700

    try non app build

---
+++ worker-index.js
@@ -0,0 +1,30 @@
+navigator.serviceWorker.register("./tile-worker.js", {
+  scope: '/',
+});
+(async function () {
+  const map = new maplibregl.Map({
+    container: "map", // container id
+    style: {
+      version: 8,
+      sources: {
+        "raster-tiles": {
+          type: "raster",
+          tiles: ["worker-server/{z}/{x}/{y}.webp"],
+          tileSize: 256,
+          maxzoom: 16,
+        },
+      },
+      layers: [
+        {
+          id: "simple-tiles",
+          type: "raster",
+          source: "raster-tiles",
+          minzoom: 0,
+          maxzoom: 22,
+        },
+      ],
+    },
+    center: [-122.56572332003616, 47.31040904747115], // starting position
+    zoom: 14,
+  });
+})();
